fb61dfeac819c9f198d97a88ada39f742c696c24
revabiz0 <49290827+re-va@users.noreply.github.com>
2019-05-04 01:21:25 +0530

    Add files via upload

---
+++ vyvo web/sw.js
@@ -0,0 +1,1 @@
+importScripts('https://r.8b.io/26978/sw-toolbox.js');toolbox.precache(['index.html'],['manifest.json']);toolbox.router.default = toolbox.cacheFirst;self.addEventListener('install', function(event) {console.log('SW: Installing service worker');});
